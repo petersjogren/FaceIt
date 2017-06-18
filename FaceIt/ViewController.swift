f91b9a6ee6e2373eda8f81fcf23776b99e26742f
Peter Sjögren <peter.sjogren@pskonsult.se>
2017-06-18 12:18:05 +0200

    Gesture recognizers

--- FaceIt/ViewController.swift
+++ FaceIt/ViewController.swift
@@ -12,10 +12,21 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var faceView: FaceView! {
         didSet {
+            let handler = #selector(FaceView.changeScale(byReactingTo:))
+            let pinchRecognizer = UIPinchGestureRecognizer(target: faceView, action: handler)
+            faceView.addGestureRecognizer(pinchRecognizer)
+            let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(toggleEyes(byReactingTo:)))
+            faceView.addGestureRecognizer(tapRecognizer)
             updateUI()
         }
     }
     
+    func toggleEyes(byReactingTo tapRecignizer: UITapGestureRecognizer) {
+        if tapRecignizer.state == .ended {
+            let eyes: FacialExpression.Eyes = (expression.eyes == .closed) ? .open : .closed
+            expression = FacialExpression(eyes: eyes, mouth: expression.mouth)
+        }
+    }
     var expression = FacialExpression(eyes: .open, mouth: .grin) {
         didSet {
             updateUI()
